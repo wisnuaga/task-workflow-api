23be121b43df4b5c19cc555f9e05a6732926bfc3
Wisnu Agastya <wisnu.aga@gmail.com>
2026-02-08 20:09:59 +0700

    add server.ts

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { buildApp } from "./app";
+
+const app = buildApp();
+
+app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
