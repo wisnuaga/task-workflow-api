db328e4f5fa0c895cac09463e082475e2222c1f9
Wisnu Agastya <wisnu.aga@gmail.com>
2026-02-08 23:47:34 +0700

    fix imports

--- src/domain/tasks/usecases/ITaskUseCase.ts
+++ src/domain/tasks/usecases/ITaskUseCase.ts
@@ -1,4 +1,4 @@
-import { Task } from "../../domain/tasks/entities/Task";
+import { Task } from "../entities/Task";
 
 export interface ITaskUseCase {
     create(task: Omit<Task, "id" | "assigneeId" | "version" | "createdAt" | "updatedAt">): Promise<Task>;
