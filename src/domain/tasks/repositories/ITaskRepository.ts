cf95427f23e1622fe38deb55258c39ac58fab591
Wisnu Agastya <wisnu.aga@gmail.com>
2026-02-09 00:52:50 +0700

    add trx on task repo interface

--- src/domain/tasks/repositories/ITaskRepository.ts
+++ src/domain/tasks/repositories/ITaskRepository.ts
@@ -1,5 +1,5 @@
 import { Task } from "../entities/Task";
 
 export interface ITaskRepository {
-    create(task: Omit<Task, "id" | "assigneeId" | "version" | "createdAt" | "updatedAt">): Promise<Task>;
+    create(task: Omit<Task, "id" | "assigneeId" | "version" | "createdAt" | "updatedAt">, tx?: any): Promise<Task>;
 }
